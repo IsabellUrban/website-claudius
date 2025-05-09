b60fca434c6ac3f17fcfff295f1b7de9ee33ed1f
Isabell <mail@@isabellurban.com>
2025-05-09 09:10:32 +0200

    first commit

--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -1,2 +1,25 @@
 import styled from "styled-components";
 import Headline from "../Headline/Headline";
+
+export default function Contact() {
+  return (
+    <ContactSection>
+      <StyledContainer>
+        <Headline headline={"contact"} />
+        </StyledContainer>
+        </ContactSection>
+    );
+ };   
+
+  const ContactSection = styled.section`
+      background-color: transparent;
+      padding: 4rem 0rem 4rem 0rem;
+      width: 100%;
+    `;
+
+    const StyledContainer = styled.div`
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+    `;
