02cfd9b5369ef72b715b747e1da2b7f622054309
Isabell <mail@@isabellurban.com>
2025-06-11 14:00:21 +0200

    add firstRender to _App.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import Layout from "@/components/Layout/Layout";
 import GlobalStyle from "../styles";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 export default function App({ Component, pageProps }) {
-  
+  const isFirstRender = useRef(true);
+
   useEffect(() => {
-    if (typeof window !== "undefined") {
+    if (typeof window !== "undefined" && isFirstRender.current) {
+      isFirstRender.current = false;
+
       if (window.location.hash) {
         history.replaceState(null, "", window.location.pathname);
         setTimeout(() => {
@@ -16,6 +19,21 @@ export default function App({ Component, pageProps }) {
       }
     }
   }, []);
+
+/*   useEffect(() => {
+    if (typeof window !== "undefined") {
+      if (window.location.hash) {
+        history.replaceState(null, "", window.location.pathname);
+        setTimeout(() => {
+          window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+        }, 1);
+      } else {
+        window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+      }
+    }
+  }, []); */
+
+ 
 
 
   return (
